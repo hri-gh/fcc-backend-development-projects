9ab0a8a91da8ee5d030d626c53234276b0234118
hri-gh <hrithikgh.edu@gmail.com>
2024-08-09 18:12:33 +0530

    "Added, boilerplate-project-headerparser."

--- request-header-parser-microservice/index.js
+++ request-header-parser-microservice/index.js
@@ -1,27 +1,50 @@
-import express from 'express';
+// index.js
+// where your node app starts
 
-const app = express();
+// init project
+require('dotenv').config();
+var express = require('express');
+var app = express();
 
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint...
+app.get('/api/hello', function (req, res) {
+  res.json({ greeting: 'hello API' });
+});
 
 
 app.get('/api/whoami', function (req, res) {
-    // Get the IP address
-    const ipaddress = req.ip;
+  // Get the IP address
+  const ipaddress = req.ip;
 
-    // Get the Accept-Language header
-    const language = req.headers['accept-language'];
+  // Get the Accept-Language header
+  const language = req.headers['accept-language'];
 
-    // Get the User-Agent header
-    const software = req.headers['user-agent'];
+  // Get the User-Agent header
+  const software = req.headers['user-agent'];
 
-    res.json({
-        ipaddress: ipaddress,
-        language: language,
-        software: software,
-    });
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software,
+  });
 });
 
-const PORT = process.env.PORT || 8000
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT || 3000, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
